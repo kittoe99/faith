3bd15f40c761687b3c0e035042c2cbe7099f6228
kittoe99 <kofikittoe35@gmail.com>
2025-07-27 17:11:09 -0600

    chore: swap Supabase for SQLite; fix webpack externals

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Skip bundling the native module; it's required at runtime in the serverless function
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('better-sqlite3');
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig
